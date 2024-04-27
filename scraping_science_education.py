4b4d58a1cd87d5d5e6251adc9489d7f7a867c34f
ali7haider <haider5568761@gmail.com>
2024-04-27 21:41:16 +0500

    Scrapped Science Education articles from StemVision

--- scraping_science_education.py
+++ scraping_science_education.py
@@ -14,23 +14,20 @@ def scrape_articles_links(url):
         response = requests.get(url)
         response.encoding = 'utf-8'  # Set the encoding to UTF-8
         soup = BeautifulSoup(response.text, 'html.parser')
-        posts_div = soup.find('div', class_='listing')
+        posts_div = soup.find('div', class_='search-main')
         
         # Initialize a list to store links
         links = []
 
         # Find all the <div> tags with class "content-box"
         if posts_div:
-            for div in posts_div.find_all('div', class_='content-box'):
+            for div in posts_div.find_all('span', class_='read-more'):
                 # Find all the <a> tags within the div
-                for h2_tag in div.find_all('h2'):
-                    # Find all the <a> tags within each <h2> tag
-                    for link in h2_tag.find_all('a'):
-                        # Get the href attribute of each <a> tag
-                        href = link.get('href')
-                        # Append the link to the list
-                        links.append(href)
-        
+                for link in div.find_all('a'):
+                    # Get the href attribute of each <a> tag
+                    href = link.get('href')
+                    # Append the link to the list
+                    links.append(href)
         return links
 
     except Exception as e:
@@ -43,8 +40,8 @@ def scrape_articles_links(url):
 def process_links(articles_links):
     unique_links = set()
     for link in articles_links:
-        if link.startswith("https://timesofindia.indiatimes.com/readersblog"):
-            unique_links.add(link)
+        full_url = "https://ssec.si.edu/" + link
+        unique_links.add(full_url)
     return list(unique_links)
 
 
@@ -56,18 +53,17 @@ def scrape_data(url):
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        article_content = soup.find('div', class_='article')
+        article_content = soup.find('div', class_='blog-content-wrapper')
         if article_content:
             article['link'] = url
             print(url)
-            article_title = article_content.find('div', class_='as-heading')
+            article_title = article_content.find('div', class_='field-title-field')
             if article_title:
                 # Remove non-ASCII characters from the title
                 article['title'] = ''.join(char for char in article_title.get_text() if ord(char) < 128)
             else:
                 article['title'] = None  # Handle the case where the title is not found
-
-            body = article_content.find('div', class_='as-wrapper')
+            body = article_content.find('div', class_='field-body')
             if body:
                 text_elements = []
 
@@ -134,15 +130,15 @@ def save_to_csv(data, filename):
 def scrape_articles(url_base, pages, min_articles):
     articles_data = []
     articles_count = 0
-    for i in range(1, pages + 1):
-        url = f"{url_base}page/{i}"
+    for i in range(0, pages + 1):
+        url = f"{url_base}?page/{i}"
         print("Page Link:" + url)
         articles_links = scrape_articles_links(url)
         links = process_links(articles_links)
         for link in links:
             data = scrape_data(link)
             if data and data.get('words_count', 0) > 500:
-                articles_data.append({'index': articles_count + 1, 'label': 'sports', **data})
+                articles_data.append({'index': articles_count + 1, 'label': 'science_education', **data})
                 articles_count += 1
                 if articles_count >= min_articles:
                     return articles_data
@@ -151,16 +147,16 @@ def scrape_articles(url_base, pages, min_articles):
 """Main function"""
 
 # URL to scrape
-sport_url = 'https://timesofindia.indiatimes.com/readersblog/sports/'
-pages = 10  # Number of pages to scrape
+sport_url = 'https://ssec.si.edu/stemvisions-blog'
+pages = 4  # Number of pages to scrape
 min_articles = 15  # Minimum number of articles to scrape
 
 # Scrape articles
 articles_data = scrape_articles(sport_url, pages, min_articles)
 # Create a directory to save the articles
 os.makedirs("articles", exist_ok=True)
-json_file = 'articles/sports_articles.json'
-csv_file = 'articles/sports_articles.csv'
+json_file = 'articles/science_education_articles.json'
+csv_file = 'articles/science_education_articles.csv'
 
 # Save to JSON file
 save_to_json(articles_data, json_file)
